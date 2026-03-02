21432206dad4d741063dbb2fa8b5191d23cb9b11
DG65 <73533984+DG65@users.noreply.github.com>
2026-03-02 15:43:51 +0100

    Add files via upload

---
+++ TessieVehicle/telemetry_maps.php
@@ -0,0 +1,9 @@
+<?php
+private const TELEMETRY_PROFILE_MAP = [
+    'CabinOverheatProtectionMode' => 'Tessie.CabinOverheatProtectionMode',
+    'CabinOverheatProtectionTemperatureLimit' => 'Tessie.CabinOverheatProtectionTempLimit'
+];
+private const TELEMETRY_TYPE_MAP = [
+    'CabinOverheatProtectionMode' => VARIABLETYPE_INTEGER,
+    'CabinOverheatProtectionTemperatureLimit' => VARIABLETYPE_INTEGER
+];
